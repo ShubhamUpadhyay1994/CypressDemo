092d602ea70e944a6f944f5dd5e17c411d50b484
unknown <shubhamupadhyay69@gmail.com>
2021-07-26 19:00:08 +0530

    second commit

---
+++ cypress/integration/test/thirdTest.ts
@@ -0,0 +1,36 @@
+import {FirstTest  } from "../page/firsttest.po";
+
+let firstPagePO = new FirstTest();
+
+//Mostly used for Setup Part
+before(function(){
+    cy.fixture('google').then(function(data)
+    {
+        this.googleSearchTestData=data ;
+    })
+    })
+
+beforeEach(function(){
+    window.console.log('Enter the beforeEach function')
+    Cypress.Cookies.preserveOnce('SESSION')
+  firstPagePO.navigate('/');
+})
+
+it('google test3-1', function(){
+    firstPagePO.enterSearchText().type(this.googleSearchTestData.searchText);
+    firstPagePO.googleImapge().type('{enter}');
+    firstPagePO.verifySearchResult().contains(this.googleSearchTestData.seearchResult);   
+})
+
+it('google test3-2', function(){
+    firstPagePO.enterSearchText().type('Hello');
+    firstPagePO.googleImapge().type('{enter}');
+    firstPagePO.verifySearchResult().contains('About');
+})
+
+it('google test3-3', function(){
+    firstPagePO.enterSearchText().type('Hello');
+    firstPagePO.googleImapge().type('{enter}');
+    firstPagePO.verifySearchResult().contains('About');
+})
+
